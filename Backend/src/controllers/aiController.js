f7bac34578c7c3055ed2177975bdf11bd0c7880b
raj kumar <rajkumarvermacea@gmail.com>
2026-08-23 21:16:37 +0530

    fix: correct function name and implement testAI controller for AI response generation

--- Backend/src/controllers/aiController.js
+++ Backend/src/controllers/aiController.js
@@ -1,8 +1,17 @@
-import ai from "../services/aiServices.js"
+// const responce =await ai.models.generateContent({
+//     model:"gemini-3.7-flash",
+//     contents:prompt
+// })
 
-const responce =await ai.models.generateContent({
-    model:"gemini-3.7-flash",
-    contents:prompt
-})
+import {generateAiResponce} from "../services/aiServices.js"
 
-export {responce}
+
+const testAI = async(req,res)=>{
+    const {message} = req.body
+    const generatedText = await generateAiResponce(message)
+    res.json(generatedText)
+}
+
+
+
+export  {testAI}
--- Backend/src/services/aiServices.js
+++ Backend/src/services/aiServices.js
@@ -4,7 +4,7 @@ const ai = new GoogleGenAI({
     apiKey:process.env.GEMINI_API_KEY
 })
 
-const genarateAiResponce =async (prompt) =>{
+const generateAiResponce =async (prompt) =>{
    const responce = await ai.models.generateContent({
     model:"gemini-3.7-flash",
     contents:prompt
@@ -12,4 +12,5 @@ const genarateAiResponce =async (prompt) =>{
     return responce.text
 }
 
-export {ai,genarateAiResponce};
+
+export {generateAiResponce};
