f3ad17ed5ca490dae90fd8ae5e808c3dd99b4116
raj kumar <rajkumarvermacea@gmail.com>
2026-08-25 00:30:27 +0530

    fix: remove commented-out console logs in resumeUpload function

--- Backend/src/controllers/aiController.js
+++ Backend/src/controllers/aiController.js
@@ -12,19 +12,17 @@ const testAI = async (req, res) => {
 const resumeUpload = async (req, res) => {
   try {
     //  const {resume} = req.file.path
-    const { jobDescription } = req.body;
-    console.log("FILE:");
-    console.dir(req.file, { depth: null });
 
-    console.log("JOB:");
-    console.log(req.body.jobDescription);
+    const { jobDescription } = req.body;
 
     const generatedText = await generateAiResponceOnResume(
       req.file,
       jobDescription,
     );
     res.json(generatedText);
+
   } catch (err) {
+
     console.error(err);
     res.status(500).json({
       message: err.message || "Internal server error",
