9868d0d256b210c9c8026334d5a1bc6daf83c3c3
raj kumar <rajkumarvermacea@gmail.com>
2026-08-25 16:02:15 +0530

    Refactor App component to use React Router and remove unused assets
    - Removed the icons.svg file as it is no longer needed.
    - Updated App.jsx to implement routing with Home component.
    - Deleted unused assets: hero.png, react.svg, and vite.svg.
    - Added a new Home component for the main page content.

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+
+    <div>Home Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima similique labore deleniti!</div>
+  )
+}
+
+export default Home
