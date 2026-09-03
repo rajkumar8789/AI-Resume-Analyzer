dfdd93d03e77ea7c33a6ae6ba59c996421b63717
raj kumar <rajkumarvermacea@gmail.com>
2026-09-03 23:42:05 +0530

    feat: implement registration form with state management and navigation

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const Login = () => {
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    name:"",
+    email: "",
+    password: "",
+  });
 
-const Register = () => {
+  const handleChange = (e) => {
+    const { name,email, password} = e.target;
+
+    setFormData({
+      ...formData,
+      [e.target.name]: [e.target.value],
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/user/register");
+    console.log(response);
+    navigate("/login")
+  };
   return (
-    <div>Register</div>
-  )
-}
+    <div className="">
+      <h2 className="text-center font-bold text-4xl">Register Page</h2>
+      <form className="flex max-w-md mx-auto p-6 bg-white rounded-lg shadow-md space-y-4">
+        <div className="flex flex-wrap">
+          <label
+            htmlFor="email"
+            className=" text-gray-700 font-medium mb-1 flex items-center"
+          >
+            Name:
+          </label>
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            id=""
+            className="w-full px-4 py-3 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 bg-slate-200"
+          />
+          <label
+            htmlFor="password"
+            className=" text-gray-700 font-medium mb-1 flex items-center"
+          >
+            Email:{" "}
+          </label>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            id=""
+            className="w-full px-4 py-3 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 bg-slate-200"
+          />
+          <label
+            htmlFor="password"
+            className=" text-gray-700 font-medium mb-1 flex items-center"
+          >
+            password:{" "}
+          </label>
+          <input
+            type="password"
+            name=""
+            onChange={handleChange}
+            id=""
+            className="w-full px-4 py-3 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 bg-slate-200"
+          />
+          <button
+            type="submit"
+            onChange={handleSubmit}
+            className="bg-amber-100 p-4 mt-3 ml-30 rounded-xl hover:bg-amber-200 text-xl font-bold cursor-pointer"
+          >
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Register
+export default Login;
