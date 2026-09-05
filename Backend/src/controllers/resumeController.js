cebd8349011814ccc12f64120ed5f50b4200b84c
raj kumar <rajkumarvermacea@gmail.com>
2026-09-05 23:59:21 +0530

    feat: add createResume route and component for resume creation functionality

---
+++ Backend/src/controllers/resumeController.js
@@ -0,0 +1,5 @@
+const createResume = (req,res)=>{
+
+}
+
+export {createResume}
--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -2,11 +2,14 @@ import express from "express"
 import {userRegister,userLogin} from "../controllers/userController.js"
 import authMiddle from "../middleware/authMiddleware.js"
 import upload from "../middleware/multer.js"
-
+import {createResume} from "../controllers/resumeController.js"
 
 const router = express.Router()
 
 router.post("/register",upload.single("profilePic"),userRegister)
+
 router.post("/login",userLogin)
+
+router.post("/create-resume",authMiddle,createResume)
 
 export default router;
